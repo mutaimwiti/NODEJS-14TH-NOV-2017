da4dfc01fa25696319123ce65f61d9de2e74674c
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-11-14 18:34:09 +0300

    Simple node app working

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-console.log("Hello Node :) I'm coming for you :(")
+/*
+Importation of modules: http, fs, url
+ */
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-var http = require("http"); http.createServer(function (request, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
+
+/*
+Creation of a server
+ */
+http.createServer(function (request, response) {
+    var query = url.parse(request.url, true);
+    if( query.pathname === "/synchronous"){
+        /*
+        Getting query from url using url module
+         */
+        var file = query.query.file;
+        /*
+        Reading a file asynchronously. Then rendering the html
+         */
+        fs.readFile("file" + file + ".html", function(err, data) {
+            if (err) {
+                response.writeHead(404, {'Content-Type': 'text/html'});
+                return response.end("404 Not Found");
+            }
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(data);
+        });
+    }
+    else{
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        /*
+        Reading a file synchronously. Then rendering html
+         */
+        var index = fs.readFileSync('index.html');
+        response.end(index);
+    }
 }).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
